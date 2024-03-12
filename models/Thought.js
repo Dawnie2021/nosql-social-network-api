0c1f399e7613c8e2ecfe99cbc8770e8eb9cafcf9
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-03-12 00:14:59 -0400

    started my thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,22 @@
 const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+
+        },
+
+        username: {
+            type: String,
+            required: true,
+        }
+    }
+)
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
