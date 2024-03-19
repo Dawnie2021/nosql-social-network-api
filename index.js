91338864e8f30493581d124b9ed3c61e43ca4b19
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-03-18 23:26:23 -0400

    took out space

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const cwd = process.cwd();
 const PORT = 3001;
 const app = express();
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,32 +1,27 @@
 const router = require('express').Router();
 
 const {
-    getThoughts,
-    getSingleThought,
-    createThought,
-    addReaction,
-    deleteReaction,
-    deleteThought,
-  } = require('../../controller/thoughtController');
-  
- 
-  router.route('/')
+  getThoughts,
+  getSingleThought,
+  createThought,
+  addReaction,
+  deleteReaction,
+  deleteThought,
+} = require('../../controller/thoughtController');
+
+router.route('/')
   .get(getThoughts)
   .post(createThought);
-  
 
-  router.route('/:thoughtId')
+router.route('/:thoughtId')
   .get(getSingleThought)
   .delete(deleteThought)
-  
 
 router.route('/:thoughtId/reactions')
-.post(addReaction)
+  .post(addReaction)
 
 router.route('/:thoughtId/reactions/:reactionId')
-.delete(deleteReaction)
+  .delete(deleteReaction)
 
 
-
-
-  module.exports = router;
+module.exports = router;
